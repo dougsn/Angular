8be4042d0e5e8430f68d25b22cb66b7edcbd1e05
Douglas Nascimento <douglassnascimet@gmail.com>
2023-07-11 11:26:43 -0300

    Finalizando aula de Validacoes Basicas de Formularios com Validators/ngIf e ngClass

--- ReactiveForms/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ ReactiveForms/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Usuario } from './models/usuario';
 
 @Component({
@@ -10,25 +10,28 @@ export class CadastroComponent implements OnInit {
 
   cadastroForm: FormGroup;
   usuario: Usuario;
+  formResult: string = '';
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.cadastroForm = this.fb.group({
-      nome: [''],
+      nome: ['', Validators.required], // Esse campo tem que ser preenchido (Validators.required)
       cpf: [''],
-      email: [''],
+      email: ['', [Validators.required, Validators.email]], // Array de validações para verificar se é requerido ou se é e-mail.
       senha: [''],
       senhaConfirmacao: ['']
     })
   }
 
   adicionarUsuario() {
-    // let x = this.cadastroForm.value; // Pegando os valores do formulário
-
-    this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value); // Preenchedo o usuário com os dados do formulário, primeiro diz qual é o objeto que se espera retornar (this.usuario), depois a referencia a fonte da informação que vai popular (this.cadastroForm.value) -> com isso o usuário será populado com os dados do Formulário.
-    console.log(this.usuario);
-    
+    if(this.cadastroForm.dirty && this.cadastroForm.valid) { // Para enviar/processar o formulario tem q estar valido e preenchido.
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value); // Preenchedo o usuário com os dados do formulário, primeiro diz qual é o objeto que se espera retornar (this.usuario), depois a referencia a fonte da informação que vai popular (this.cadastroForm.value) -> com isso o usuário será populado com os dados do Formulário.
+      console.log(this.usuario);
+      this.formResult = JSON.stringify(this.cadastroForm.value);
+    } else {
+      this.formResult = "Não submeteu!!!"
+    }
     
   }
 
@@ -41,3 +44,31 @@ export class CadastroComponent implements OnInit {
 // (ngSubmit)="adicionarUsuario()" -> Colocar no formulário para poder executar a função adicionarUsuario() quando o form foi submetido.
 
 // O FormBuilder é um boa prática de se trabalhar com formulários.
+
+// Validação básica de formulário
+
+/*
+ - Colocar o Validators.required no campo do group do FormBuilder do formulário especificado.
+
+ - Usar a diretiva *ngIf para verificar se o campo está com erros, se foi preenchido(dirty) ou se foi "tocado"(touched), com essas condições básicas, o span com a mensagem de erro aparece.
+
+ - E colocar no input um [ngClass] com a mesma validação, para mudar a classe do input e deixa-lo vermelho, indicando um erro no preenchimento.
+  
+ * Isso é em cada input *
+
+ - Para desabilitar o botão de submiter o formulário e deixa-lo sob responsabilidade do Angular, basta colocar o seguinte no btn
+    -> [disabled]="!cadastroForm.valid" => Se o formulário não estiver válido, deixa o botão desabilitado.
+
+ - Para múltiplas validações em um campo, criar um Array de Validators para no FormBuilder ([Validators.required, Validators.email]).
+   E no arquivo html criar 2 spans para cada erro (*ngIf="cadastroForm.get('email')?.errors?.required) ou *ngIf="cadastroForm.get('email')?.errors?.email) o ? antes e depois do erros serve para não ocasionar um erro null.
+
+
+*/
+
+// <span class="text-danger" *ngIf="cadastroForm.get('nome').errors && 
+// (cadastroForm.get('nome').dirty || cadastroForm.get('nome').touched)">
+// <p>Preencha o campo nome</p>
+// </span>
+
+// [ngClass]="{'is-invalid': cadastroForm.get('nome').errors && 
+// (cadastroForm.get('nome').dirty || cadastroForm.get('nome').touched)}"/>

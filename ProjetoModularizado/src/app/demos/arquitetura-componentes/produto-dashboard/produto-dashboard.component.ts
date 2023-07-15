fe48f22851a278f015c95925c5a2c264da15838f
Douglas Nascimento <douglassnascimet@gmail.com>
2023-07-15 13:53:37 -0300

    Finalizando aula de @ViewChild e @ViewChildren

--- ProjetoModularizado/src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
+++ ProjetoModularizado/src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Produto } from '../models/produto';
+import { Observable, fromEvent } from 'rxjs';
+import { ProdutoCountComponent } from './components/produto-count.component';
+import { ProdutoDetalheComponent } from './components/produto-card-detalhe.component';
 
 @Component({
   selector: 'app-produto-dashboard',
   templateUrl: './produto-dashboard.component.html',
   styles: []
 })
-export class ProdutoDashboardComponent implements OnInit {
+export class ProdutoDashboardComponent implements OnInit, AfterViewInit {
 
   produtos: Produto[] // Lista de produto que é passando para o component Filho
+
+  @ViewChild(ProdutoCountComponent, { static: false }) contador: ProdutoCountComponent;
+  @ViewChild('teste', { static: false }) mensagemTela: ElementRef; // Pegando a referencia do token #teste, que foi colocado no HTML deste component.
+  @ViewChildren(ProdutoDetalheComponent) botoes: QueryList<ProdutoDetalheComponent>; // Lista do elemento ProdutoDetalheComponent.
 
   constructor() { }
 
@@ -55,6 +62,24 @@ export class ProdutoDashboardComponent implements OnInit {
       valor: 600,
       imagem: 'headset.jpg'
     }];
+  }
+  
+  ngAfterViewInit(): void {
+
+    console.log(`Objeto do Contador: ${this.contador.produtos}`);
+
+
+    let clickTexto: Observable<any> = fromEvent(this.mensagemTela.nativeElement, 'click'); // Observando o evento click do elemento mensagemTela
+    clickTexto.subscribe(() => {
+      alert('clicou no texto')
+      return;
+    });
+
+    console.log(this.botoes);
+    this.botoes.forEach(p => {
+      console.log(p.produto);     
+    })
+    
   }
 
   mudarStatus(event: Produto){ // Se estiver ativo vai para inativo e vise-versa.

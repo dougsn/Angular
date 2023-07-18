0351399f4bc61637b169c85f4fa0247c4e2079c2
Douglas Nascimento <douglassnascimet@gmail.com>
2023-07-18 10:04:10 -0300

    Resolvendo findById por dados que vem pela rota, sem precisar ir ao backend

--- RoteamentoAvancado/src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
+++ RoteamentoAvancado/src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
@@ -3,6 +3,7 @@ import { Produto } from '../models/produto';
 import { Observable, fromEvent } from 'rxjs';
 import { ProdutoCountComponent } from './components/produto-count.component';
 import { ProdutoDetalheComponent } from './components/produto-card-detalhe.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-produto-dashboard',
@@ -11,57 +12,17 @@ import { ProdutoDetalheComponent } from './components/produto-card-detalhe.compo
 })
 export class ProdutoDashboardComponent implements OnInit, AfterViewInit {
 
+  constructor(private route: ActivatedRoute){}
+
   produtos: Produto[] // Lista de produto que é passando para o component Filho
 
   @ViewChild(ProdutoCountComponent, { static: false }) contador: ProdutoCountComponent;
   @ViewChild('teste', { static: false }) mensagemTela: ElementRef; // Pegando a referencia do token #teste, que foi colocado no HTML deste component.
   @ViewChildren(ProdutoDetalheComponent) botoes: QueryList<ProdutoDetalheComponent>; // Lista do elemento ProdutoDetalheComponent.
 
-  constructor() { }
 
   ngOnInit() {
-    this.produtos = [{
-      id: 1,
-      nome: 'Teste',
-      ativo: true,
-      valor: 100,
-      imagem: 'celular.jpg'
-    },
-    {
-      id: 2,
-      nome: 'Teste 2',
-      ativo: true,
-      valor: 200,
-      imagem: 'gopro.jpg'
-    },
-    {
-      id: 3,
-      nome: 'Teste 3',
-      ativo: true,
-      valor: 300,
-      imagem: 'laptop.jpg'
-    },
-    {
-      id: 4,
-      nome: 'Teste 4',
-      ativo: true,
-      valor: 400,
-      imagem: 'mouse.jpg'
-    },
-    {
-      id: 5,
-      nome: 'Teste 5',
-      ativo: true,
-      valor: 500,
-      imagem: 'teclado.jpg'
-    },
-    {
-      id: 6,
-      nome: 'Teste 6',
-      ativo: false,
-      valor: 600,
-      imagem: 'headset.jpg'
-    }];
+    this.produtos = this.route.snapshot.data['produtos'];
   }
   
   ngAfterViewInit(): void {
--- RoteamentoAvancado/src/app/demos/arquitetura-componentes/services/produto.service.ts
+++ RoteamentoAvancado/src/app/demos/arquitetura-componentes/services/produto.service.ts
@@ -52,7 +52,10 @@ export class ProdutoService {
         }];
     }
 
-    obterTodos(): Produto[] {
+    obterTodos(estado: string): Produto[] {
+        if(estado === 'ativos') {
+            return this.produtos.filter(produto => produto.ativo);
+        }
         return this.produtos;
     }
 

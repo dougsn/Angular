83f17c9c0de725dde6955a0d397111fa5dc676b9
Douglas Nascimento <douglassnascimet@gmail.com>
2023-07-19 10:53:53 -0300

    Finalizando aula de useFactory

--- DI e Zonas/src/app/demos/bar-di-zones/bar-di-zones.component.ts
+++ DI e Zonas/src/app/demos/bar-di-zones/bar-di-zones.component.ts
@@ -1,12 +1,18 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { BarServices } from './bar.service';
+import { Component, Inject, Injector, OnInit } from '@angular/core';
+import { BarFactory, BarServices } from './bar.service';
 import { BAR_UNIDADE_CONFIG, BarUnidadeConfig } from './bar.config';
 
 @Component({
   selector: 'app-bar-di-zones',
   templateUrl: './bar-di-zones.component.html',
   providers: [
-    { provide: BarServices, useClass: BarServices }
+    // { provide: BarServices, useClass: BarServices },
+    {
+      provide: BarServices, useFactory: BarFactory, 
+      deps: [
+        Injector
+      ]
+    }
   ]
 })
 export class BarComponent implements OnInit {
@@ -14,6 +20,7 @@ export class BarComponent implements OnInit {
   configManual: BarUnidadeConfig;
   config: BarUnidadeConfig;
   barBebida1: string;
+  dadosUnidade: string;
 
   constructor( 
     private barServices: BarServices,
@@ -26,6 +33,7 @@ export class BarComponent implements OnInit {
     this.configManual = this.ApiConfigManual;
     
     this.config = this.ApiConfig;
+    this.dadosUnidade = this.barServices.obterUnidades();
   }
 
 }
--- DI e Zonas/src/app/demos/bar-di-zones/bar.service.ts
+++ DI e Zonas/src/app/demos/bar-di-zones/bar.service.ts
@@ -1,12 +1,20 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http"
+import { Inject, Injectable, Injector } from "@angular/core";
+import { BAR_UNIDADE_CONFIG, BarUnidadeConfig } from "./bar.config";
 
+
+export function BarFactory(inject: Injector) {
+    return new BarServices(inject.get(BAR_UNIDADE_CONFIG));
+}
 
 @Injectable()
 export class BarServices {
 
-    constructor() { }
+    constructor(@Inject(BAR_UNIDADE_CONFIG) private config: BarUnidadeConfig) { }
 
+    obterUnidades(): string {
+        return `Unidade ID: ${this.config.unidadeId} - Token: ${this.config.unidadeToken}`;
+    }
+    
     obterBebidas(): string {
         return 'Bebidas';
     }

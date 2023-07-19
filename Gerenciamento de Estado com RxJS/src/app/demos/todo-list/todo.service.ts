d6d418e2640b7b6a522c84ee91df4d6f8f01ff77
Douglas Nascimento <douglassnascimet@gmail.com>
2023-07-19 15:57:52 -0300

    INiciando aula de utilizando um serviço

--- Gerenciamento de Estado com RxJS/src/app/demos/todo-list/todo.service.ts
+++ Gerenciamento de Estado com RxJS/src/app/demos/todo-list/todo.service.ts
@@ -8,6 +8,22 @@ import { Task } from './task';
 export class TasksService {
 
   constructor(private http: HttpClient) { }
+  
+  
+  // Criando uma propriedade que irá retorar uma lista de Tasks pegando do endereço informado
+  // É a forma que consumimos informações do backend.
+  getTodoList$ : Observable<Task[]> = this.http
+    .get<Task[]>('http://localhost:3000/todolist');
+
+
+  // Essa é a forma feita sem o $ no final.. deixando menos clean.
+
+  // getToDoList() : Observable<Task[]> {
+  //   return this.http
+  //   .get<Task[]>('http://localhost:3000/todolist')
+  // }
+
+
 
   
 }

7daebc338aa14cd95846f3a44fb603ee56ad315e
Douglas Nascimento <douglassnascimet@gmail.com>
2023-07-19 11:26:11 -0300

    Utilizando NgZones para executar processos dentro e fora da zona do angular

--- DI e Zonas/src/app/demos/bar-di-zones/bar-di-zones.component.ts
+++ DI e Zonas/src/app/demos/bar-di-zones/bar-di-zones.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Injector, OnInit } from '@angular/core';
+import { Component, Inject, Injector, NgZone, OnInit } from '@angular/core';
 import { BarFactory, BarServices, BebidaService } from './bar.service';
 import { BAR_UNIDADE_CONFIG, BarUnidadeConfig } from './bar.config';
 
@@ -8,7 +8,7 @@ import { BAR_UNIDADE_CONFIG, BarUnidadeConfig } from './bar.config';
   providers: [
     // { provide: BarServices, useClass: BarServices },
     {
-      provide: BarServices, useFactory: BarFactory, 
+      provide: BarServices, useFactory: BarFactory,
       deps: [
         Injector
       ]
@@ -24,21 +24,57 @@ export class BarComponent implements OnInit {
   barBebida2: string;
   dadosUnidade: string;
 
-  constructor( 
+  constructor(
     private barServices: BarServices,
     @Inject('ConfigManualUnidade') private ApiConfigManual: BarUnidadeConfig,
     @Inject(BAR_UNIDADE_CONFIG) private ApiConfig: BarUnidadeConfig,
-    private bebidaService: BebidaService
-   ) { }
+    private bebidaService: BebidaService,
+    private ngZone: NgZone
+  ) { }
 
   ngOnInit() {
     this.barBebida1 = this.barServices.obterBebidas();
     this.configManual = this.ApiConfigManual;
-    
+
     this.config = this.ApiConfig;
     this.dadosUnidade = this.barServices.obterUnidades();
 
     this.barBebida2 = this.bebidaService.obterBebidas();
+
+  }
+
+  public progress: number = 0;
+  public label: string;
+
+  processWithinAngularZone() {
+    this.label = 'dentro';
+    this.progress = 0;
+    this._increaseProgress(() => console.log("Finalizado por dentro do Anular"));
+  }
+
+  processOutsideOfAngularZone() {
+    this.label = 'fora';
+    this.progress = 0;
+    this.ngZone.runOutsideAngular(() => {
+      this._increaseProgress(() => {
+        this.ngZone.run(() => {
+          console.log("Finalizado Fora do Angular!!");
+        })
+      })
+    })
+  }
+
+
+
+  _increaseProgress(doneCallBack: () => void) {
+    this.progress += 1;
+    console.log(`Progresso atual: ${this.progress}%`);
+
+    if (this.progress < 100) {
+      window.setTimeout(() => this._increaseProgress(doneCallBack), 10);
+    } else {
+      doneCallBack();
+    }
 
   }
 
